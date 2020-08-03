8e6de14460b004ddcdda960e7532827c7165022d
Mauro <mauro.spinosi94@gmail.com>
2020-08-03 17:00:50 +0200

    Esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 // Inserisci il nome//
 var nome = prompt("Inserisci il nome");
-document.getElementById('nome').innerHTML = nome;
-
 // Inserisci il cognome//
 var cognome = prompt("Inserisci il cognome");
-document.getElementById('cognome').innerHTML = cognome;
-
 // Inserisci il colore preferito//
 var colorePreferito = prompt("Inserisci il colore preferito");
-document.getElementById('colorepreferito').innerHTML = colorePreferito;
+document.getElementById('password').innerHTML = nome + " " + cognome + " "  + colorePreferito + "19";
