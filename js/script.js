de6ab639353052d63be6791f37215d85805dab1c
Mauro <mauro.spinosi94@gmail.com>
2020-08-03 16:47:27 +0200

    nome, cognome , colore

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-var nome = promt("Inserisci il nome");
-var cognome = promt("Inserisci il cognome");
-var colorePreferito = promt("Inserisci il colore preferito");
+// Inserisci il nome//
+var nome = prompt("Inserisci il nome");
+document.getElementById('nome').innerHTML = nome;
+
+// Inserisci il cognome//
+var cognome = prompt("Inserisci il cognome");
+document.getElementById('cognome').innerHTML = cognome;
+
+// Inserisci il colore preferito//
+var colorePreferito = prompt("Inserisci il colore preferito");
+document.getElementById('colorepreferito').innerHTML = colorePreferito;
